4aeb90e443c6bc1f67866fc60891d1c6f11783cf
caioabrahao <contato.caioabrahao@gmail.com>
2024-10-24 16:54:32 -0300

    first commit

---
+++ run.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def frontpage():
+    return render_template('frontpage.html')
+
+@app.route('/cadastrar')
+def cadastrar():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/varpage')
+def varpage():
+    return render_template('new_event.html')
+
+app.run(debug=True)
