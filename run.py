196013c48adb34a783ba4d7ba51a6f5cc208185f
caioabrahao <contato.caioabrahao@gmail.com>
2024-10-25 08:58:05 -0300

    created home page frontend

--- run.py
+++ run.py
@@ -13,8 +13,34 @@ def cadastrar():
 def login():
     return render_template('login.html')
 
+
+
 @app.route('/varpage')
 def varpage():
+    return render_template('bet_on_event.html')
+
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/my_wallet')
+def my_wallet():
     return render_template('frontpage.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('frontpage.html')
+
+@app.route('/search_events')
+def search_events():
+    return render_template('frontpage.html')
+
+@app.route('/add_new_event')
+def add_new_event():
+    return render_template('frontpage.html')
+
+
 
 app.run(debug=True)
